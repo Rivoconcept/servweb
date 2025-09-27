265edf2ce2f9b2a4300263f0c643493700a7c3a6
Rivo Vonjiniaina Hanitrarivelo <rhanitra@c1r12s9.42antananarivo.mg>
2025-09-27 14:36:27 +0300

    cours php

---
+++ webserv/clients/www1/cgi_bin/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$name = 'rivo';
+$note1 = 20;
+$note3 = 10;
+$moyenne = ($note1 + $note3) / 2;
+
+echo "Bonjour $name Votre moyenne est de $moyenne";
+?>
--- webserv/www/html1/cgi-bin/test.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "<h1>CGI Test OK</h1>";
-echo "<h2>PHP is working fine</h2>";
-?>
