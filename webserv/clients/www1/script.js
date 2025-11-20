d7e371b2c4349a07f43dc7507a91d468a1c63ef6
Rivo Vonjiniaina Hanitrarivelo <rhanitra@c1r5s8.42antananarivo.mg>
2025-11-20 18:55:21 +0300

    js

--- webserv/clients/www1/script.js
+++ webserv/clients/www1/script.js
@@ -1,6 +1,6 @@
 document.getElementById('deleteBtn').addEventListener('click', function()
 {
-    var path = document.getElementById('filePath').value.trim();
+    let path = document.getElementById('filePath').value.trim();
     if (!path) {
         alert('Please enter a file path.');
         return;
@@ -24,3 +24,64 @@ document.getElementById('deleteBtn').addEventListener('click', function()
         document.getElementById('deleteResult').textContent = "Error : " + err;
     });
 });
+
+/*function getRandomInt(max)
+{
+    return (Math.floor(Math.random() * (max + 1)));
+}
+
+const intRandom = getRandomInt(10);
+console.log(intRandom);
+
+function isRight(n)
+{
+    return (intRandom === n);
+}
+
+function guess()
+{
+    const number = parseInt(prompt("Enter a number:"), 10);
+    return (isRight(number));
+}
+
+for (let i = 0; i < 3; i++)
+{
+    if (guess())
+    {
+        console.log("bravo")
+        break;
+    } else if (i === 2)
+    {
+        console.log("you lose");
+    }
+}*/
+
+
+/*function isPremier(n)
+{
+    if (n < 2)
+    {
+        return (false);
+    }
+    for (let i = n - 1; i > 1; i--)
+    {
+        if (n % i === 0)
+        {
+            return (false)
+        }
+    }
+    return (true)
+}
+
+console.log('0', isPremier(0));
+console.log('11', isPremier(11));
+console.log('12', isPremier(12));*/
+
+
+function isPalendrome(word)
+{
+    const reverseWord = word
+        .split-
+}
+
+console.log(reverse("Salut"));
