82c987ba0377ee074f043c9eca4cb727e99e9a98
Rivo Vonjiniaina Hanitrarivelo <rhanitra@c1r10s5.42antananarivo.mg>
2025-12-07 15:13:13 +0300

    modif main

--- webserv/clients/www1/cgi_bin/loop.php
+++ webserv/clients/www1/cgi_bin/loop.php
@@ -1,15 +1,16 @@
 <?php
-// script qui boucle mais se protège avec set_time_limit pour ne pas bloquer indéfiniment
-set_time_limit(5);
-header('Content-Type: text/plain');
-echo "Start loop...\n";
-$start = time();
-while (true) {
-    // sleep to avoid busy loop
-    sleep(1);
-    echo "tick\n";
-    flush();
-    if (time() - $start > 4) break; // safety: break after ~4s
-}
-echo "Done\n";
+
+    set_time_limit(5);
+    header('Content-Type: text/plain');
+    echo "Start loop...\n";
+    $start = time();
+    while (true) {
+        sleep(1);
+        echo "tick\n";
+        flush();
+        if (time() - $start > 4)
+            break;
+    }
+    echo "Done\n";
+    
 ?>
